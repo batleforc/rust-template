56fe4a570ffb7e276a252fc4d242e8998216689e
Maxime Leriche <maxime.leriche@macif.fr>
2023-02-12 03:11:54 +0100

    Feat : Mise en place de la création de la table user

--- src/model/user.rs
+++ src/model/user.rs
@@ -15,3 +15,24 @@ pub struct User {
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
 }
+
+impl User {
+    pub async fn create_table(pool: deadpool_postgres::Pool) -> Result<u64, tokio_postgres::Error> {
+        let client = pool.get().await.unwrap();
+
+        let create_table = "
+            CREATE TABLE IF NOT EXISTS users (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                nom VARCHAR(255) NOT NULL,
+                prenom VARCHAR(255) NOT NULL,
+                otp_secret VARCHAR(255),
+                otp_url VARCHAR(255),
+                otp_enabled BOOLEAN DEFAULT FALSE,
+                created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+            );";
+        client.execute(create_table, &[]).await
+    }
+}
