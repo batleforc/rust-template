669c2c6f43186436864f0626a2c3ab6b1358343d
Maxime Leriche <maxime.leriche@macif.fr>
2023-02-16 01:27:59 +0100

    Feat : Setup token for login

--- src/route/auth/login.rs
+++ src/route/auth/login.rs
@@ -3,7 +3,10 @@ use deadpool_postgres::Pool;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use crate::model::user::User;
+use crate::model::{
+    token::{RefreshToken, TokenClaims},
+    user::User,
+};
 
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct LoginUser {
@@ -14,7 +17,6 @@ pub struct LoginUser {
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct LoginUserReturn {
     pub user: User,
-    pub token: String,
     pub refresh_token: String,
 }
 
@@ -31,7 +33,7 @@ pub struct LoginUserReturn {
 pub async fn login(login_body: web::Json<LoginUser>, db_pool: web::Data<Pool>) -> impl Responder {
     let body = login_body.into_inner();
     let pool: Pool = db_pool.into_inner().as_ref().clone();
-    let user = match User::get_one_by_mail(pool, body.email.clone()).await {
+    let user = match User::get_one_by_mail(pool.clone(), body.email.clone()).await {
         Ok(user) => {
             tracing::debug!(user = ?body.email.clone() ,"User found");
             user
@@ -60,5 +62,32 @@ pub async fn login(login_body: web::Json<LoginUser>, db_pool: web::Data<Pool>) -
         "User logged in, generating refresh_token"
     );
 
-    HttpResponse::Ok().finish()
+    let refresh_token = match TokenClaims::new_tokens(user.id, true, None) {
+        Ok(token) => token,
+        Err(err) => {
+            tracing::error!(error = ?err,user = ?body.email.clone() ,"Error while generating token");
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+
+    let refresh_token_db = RefreshToken {
+        created_at: chrono::Utc::now(),
+        user_id: user.id,
+        token: refresh_token.clone(),
+    };
+
+    match refresh_token_db.create(pool.clone()).await {
+        Ok(_) => {
+            tracing::debug!(user = ?body.email.clone() ,"Refresh token saved to db");
+        }
+        Err(err) => {
+            tracing::error!(error = ?err,user = ?body.email.clone() ,"Error while saving refresh token to db");
+            return HttpResponse::InternalServerError().finish();
+        }
+    }
+
+    HttpResponse::Ok().json(LoginUserReturn {
+        user,
+        refresh_token: refresh_token,
+    })
 }
